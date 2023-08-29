804a08cd6b223db2df124d25a90fa9d535fe5a43
electroae4 <electro.ae4@gmail.com>
2023-08-29 17:04:38 +0200

    Solution of tasks 41-50

---
+++ basic-javascript/48-shopping-list.js
@@ -0,0 +1,7 @@
+const myList = [];
+myList.push(["Apple", 5]);
+myList.push(["Peach", 2]);
+myList.push(["Nut", 15]);
+myList.push(["Potato", 12]);
+myList.push(["Pepper", 1]);
+myList.push(["Carrot", 3]);

9d91352997f61074faab58700db726541d518ecc
electroae4 <electro.ae4@gmail.com>
2023-08-29 22:59:54 +0200

    Solution of tasks 91-100

---
+++ basic-javascript/92-manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Filya Kirkorov",
+    "title": "Million alyh roz",
+    "release_year": 1990,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": false
+  }
+];
